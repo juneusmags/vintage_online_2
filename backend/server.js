01223f93e7d33a582e31dfbf30de916b857401df
Juneus Magsakay <jbm9397@gmail.com>
2021-12-27 17:48:15 -0500

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -39,9 +39,6 @@ dotenv.config()
 connectDB()
 
 
-app.get('/', (req, res) =>{
-    res.send('API is running...')
-})
 
 
 app.use('/api/products', productRoutes)
@@ -54,7 +51,19 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+}
+else{
+    app.get('/', (req, res) =>{
+        res.send('API is running...')
+    })
+    
+}
 
+
+
+app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
 app.use(notFound)
 
 
